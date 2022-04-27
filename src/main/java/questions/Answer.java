650854df733ef3efcfc67739fccbf40981667328
Lady Valderrama <ladymarcela96@hotmail.com>
2022-04-26 21:50:13 -0500

    Final de la automatizacion

--- src/main/java/questions/Answer.java
+++ src/main/java/questions/Answer.java
@@ -1,12 +1,14 @@
 package questions;
+
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Question;
 import net.serenitybdd.screenplay.questions.Text;
 import userinterface.SearchPage;
 
-public class Answer implements Question<Boolean>{
+public class Answer implements Question<Boolean> {
     private String question;
-    public Answer(String question){
+
+    public Answer(String question) {
         this.question = question;
     }
 
@@ -17,12 +19,11 @@ public class Answer implements Question<Boolean>{
     @Override
     public Boolean answeredBy(Actor actor) {
         boolean result;
-        Double totalShoes = Text.of(SearchPage.TOTAL).viewedBy(actor).asDouble();
-        System.out.println("TOTAL DE ZAPATOS");
-        System.out.println(totalShoes);
-        if(question.equals(totalShoes)){
+        String numbStr = Text.of(SearchPage.TOTAL).viewedBy(actor).asString();
+        float totalShoes = Float.parseFloat(numbStr.replaceAll("\\.", ""));
+        if (totalShoes > Float.parseFloat(question)) {
             result = true;
-        }else{
+        } else {
             result = false;
         }
         return result;
--- src/main/java/userinterface/SearchPage.java
+++ src/main/java/userinterface/SearchPage.java
@@ -8,5 +8,5 @@ public class SearchPage extends PageObject {
     public static final Target SEARCH = Target.the("search bar").located(By.id("gh-ac"));
     public static final Target BUTTON_SEARCH = Target.the("button").located(By.id("gh-btn"));
     public static final Target CHECK = Target.the("check").located(By.xpath("//*[@name='Brand'][1]"));
-    public static final Target TOTAL = Target.the("total").located(By.xpath("//span[contains(text(),'670.000')]"));
+    public static final Target TOTAL = Target.the("total").located(By.xpath("/html[1]/body[1]/div[5]/div[6]/div[1]/div[1]/div[2]/div[1]/div[1]/h1[1]/span[1]"));
 }
